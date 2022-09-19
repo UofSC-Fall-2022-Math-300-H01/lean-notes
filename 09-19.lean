130e3365449fc44b30f3a244bacd9018b233438f
Matthew Ballard <matt@mrb.email>
2022-09-19 14:51:15 -0400

    add an example from hw2

--- 09-19.lean
+++ 09-19.lean
@@ -4,7 +4,7 @@ theorem problem1 (A B : Prop) : (A → B) → (¬ B → ¬ A) := sorry
 
 theorem problem2 (A B : Prop) : ¬ (A ∨ B) ↔ ¬ A ∧ ¬ B := sorry
 
-theorem problem3 {A B C : Prop} (h : B ↔ C) : (A ∨ B) ↔ A ∨ C := sorry
+theorem problem3 (A B C : Prop) (h : B ↔ C) : (A ∨ B) ↔ A ∨ C := sorry
 
 theorem problem4 (A B : Prop) : (A → B) ↔ ¬ A ∨ B := sorry 
 
